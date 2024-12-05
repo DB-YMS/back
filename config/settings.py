c23036e1f8a16edf4b0c3c4444f07d05b5fd768f
ahn2y <myhand1207@gmail.com>
2024-12-05 17:12:53 +0900

    fix : dir 수정

--- config/settings.py
+++ config/settings.py
@@ -12,10 +12,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 
+# 프로젝트 루트 경로를 Python Path에 추가
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+'''
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+'''
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
