76069708ed14c60cd0d3dd712112980b64d5f182
ahn2y <myhand1207@gmail.com>
2024-12-05 16:26:28 +0900

    feat : 설정 추가

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from location import views
 
 from rest_framework.authtoken import views
@@ -47,3 +49,8 @@ urlpatterns = [
     path('api/driver_list/<int:driver_id>', DriverDetail.as_view(), name='driver_list'),
     path('api/auth', views.obtain_auth_token, name='obtain_auth_token'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
